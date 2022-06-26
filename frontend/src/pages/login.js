6031d29606f53aafa88232724fd5f7fbef5afde9
Adelia08 <khffhadelia@gmail.com>
2022-06-26 12:42:22 +0700

    chore : sync login dan register

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -8,101 +8,140 @@ import {
   Stack,
   Image,
   Text,
+  Select,
 } from "@chakra-ui/react";
-import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { Fragment, useState } from "react";
+import axios from "axios";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [msg, setMsg] = useState("");
-  const navigate = useNavigate();
+  const [redirect, setRedirect] = useState(false);
 
-  useEffect(() => {
-    if (localStorage.getItem("admin")) {
-    }
-  }, [navigate]);
+  const loginAdmin = () => {
+    const data = {
+      username: username,
+      password: password,
+    };
+    axios
+      .post("https://csslearn.ilhamadhim.me/api/admin/login", data)
+      .then((result) => {
+        if (result) {
+          localStorage.setItem("token", result.data.token);
+          setRedirect(true);
+        }
+        console.log(result.data.token);
+      });
+  };
 
-  async function login() {
-    console.warn(username, password);
-    let item = { username, password };
-    let result = await fetch("https://csslearn.ilhamadhim.me/api/admin/login", {
-      method: "POST",
-      headers: {
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(item),
-    });
-    result = await result.json();
-    localStorage.setItem("admin", JSON.stringify(result));
-    navigate("/admin/course");
-  }
+  const loginUser = () => {
+    const data = {
+      username: username,
+      password: password,
+    };
+    axios
+      .post("https://csslearn.ilhamadhim.me/api/user/login", data)
+      .then((result) => {
+        if (result) {
+          localStorage.setItem("token", result.data.token);
+          setRedirect(true);
+        }
+        console.log(result.data.token);
+      });
+  };
 
   return (
-    <Stack
-      bg={"white"}
-      minH={"100vh"}
-      direction={{ base: "column", md: "row" }}
-    >
-      <Flex p={8} flex={1} align={"center"} justify={"center"}>
-        <Stack spacing={4} w={"full"} maxW={"md"}>
-          <Heading color={"#205375"} fontSize={"2xl"}>
-            Get Started
-          </Heading>
-          <FormControl color={"#205375"} id="username">
-            <FormLabel>Username</FormLabel>
-            <Input
-              borderColor={"#205375"}
+    <Fragment>
+      {redirect && <Navigate to="/admin/course" />}
+      {redirect && <Navigate to="/siswa" />}
+      <Stack
+        bg={"white"}
+        minH={"100vh"}
+        direction={{ base: "column", md: "row" }}
+      >
+        <Flex p={8} flex={1} align={"center"} justify={"center"}>
+          <Stack spacing={4} w={"full"} maxW={"md"}>
+            <Heading color={"#205375"} fontSize={"2xl"}>
+              Get Started
+            </Heading>
+            {/* <Select
+              size="md"
+              width={40}
+              color="gray.400"
               variant="outline"
-              color="teal"
-              placeholder="username"
-              _placeholder={{ color: "inherit" }}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </FormControl>
-          <FormControl color={"#205375"} id="password">
-            <FormLabel>Password</FormLabel>
-            <Input
-              borderColor={"#205375"}
-              variant="outline"
-              color="teal"
-              placeholder="password"
-              _placeholder={{ color: "inherit" }}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
-          <Stack spacing={6}>
-            <Stack
-              direction={{ base: "column", sm: "row" }}
-              align={"start"}
-              justify={"space-between"}
-            ></Stack>
-            <Button onClick={login} colorScheme={"blue"} variant={"solid"}>
-              Sign In
-            </Button>
-            <Text color={"#205375"} flex={"1"} align={"center"}>
-              Not registered yet?
-            </Text>
-            <Link to="/register">
-              <Button colorScheme={"blue"} variant={"solid"}>
-                Sign Up
+              borderColor={"#33A9DC"}
+              placeholder="Pilih Role ..."
+            >
+              <option value="admin">Admin</option>
+              <option value="user">Siswa</option>
+            </Select> */}
+
+            <Text></Text>
+            <FormControl color={"#205375"} id="username">
+              <FormLabel>Username</FormLabel>
+              <Input
+                borderColor={"#205375"}
+                variant="outline"
+                color="teal"
+                placeholder="username"
+                _placeholder={{ color: "inherit" }}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </FormControl>
+            <FormControl color={"#205375"} id="password">
+              <FormLabel>Password</FormLabel>
+              <Input
+                borderColor={"#205375"}
+                variant="outline"
+                color="teal"
+                placeholder="password"
+                _placeholder={{ color: "inherit" }}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormControl>
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              ></Stack>
+              <Button
+                onClick={loginAdmin}
+                colorScheme={"blue"}
+                variant={"solid"}
+              >
+                Sign In Admin
               </Button>
-            </Link>
+              <Button
+                onClick={loginUser}
+                colorScheme={"blue"}
+                variant={"solid"}
+              >
+                Sign In user
+              </Button>
+              <Text color={"#205375"} flex={"1"} align={"center"}>
+                Not registered yet?
+              </Text>
+              <Link to="/register">
+                <Button colorScheme={"blue"} variant={"solid"}>
+                  Sign Up
+                </Button>
+              </Link>
+            </Stack>
           </Stack>
-        </Stack>
-      </Flex>
-      <Flex bg={"#205375"} flex={1}>
-        {/* <Image alt={"logo"} src="https://i.ibb.co/412P9mk/Group-30.png" /> */}
-        <Image
-          alt={"Login Image"}
-          objectFit={"cover"}
-          src={
-            "https://i.ibb.co/jGk9x4j/3d-flame-business-woman-using-a-phone-with-fingerprint-scanner.png"
-          }
-        />
-      </Flex>
-    </Stack>
+        </Flex>
+        <Flex bg={"#205375"} flex={1}>
+          {/* <Image alt={"logo"} src="https://i.ibb.co/412P9mk/Group-30.png" /> */}
+          <Image
+            alt={"Login Image"}
+            objectFit={"cover"}
+            src={
+              "https://i.ibb.co/jGk9x4j/3d-flame-business-woman-using-a-phone-with-fingerprint-scanner.png"
+            }
+          />
+        </Flex>
+      </Stack>
+    </Fragment>
   );
 }
--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -9,112 +9,109 @@ import {
   Image,
   Text,
 } from "@chakra-ui/react";
-import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { useState, Fragment } from "react";
 import axios from "axios";
 
 export default function Register() {
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [msg, setMsg] = useState("");
-  // const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
-  // const Register = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     await axios.post("https://csslearn.ilhamadhim.me/api/user/register", {
-  //       name: name,
-  //       email: email,
-  //       password: password,
-  //     });
-  //     localStorage.setItem('token', response.data.token);
-  //     navigate.push("/login");
-  //   } catch (error) {
-  //     if (error.response) {
-  //       console.log(error.response.data);
-  //     }
-  //   }
-  // };
-
+  const register = () => {
+    const data = {
+      username: username,
+      email: email,
+      password: password,
+    };
+    axios
+      .post("https://csslearn.ilhamadhim.me/api/user/register", data)
+      .then((result) => {
+        if (result) {
+          localStorage.setItem("token", result.data.token);
+          setRedirect(true);
+        }
+        console.log(result.data.token);
+      });
+  };
   return (
-    <Stack
-      bg={"white"}
-      minH={"100vh"}
-      direction={{ base: "column", md: "row" }}
-    >
-      <Flex p={8} flex={1} align={"center"} justify={"center"}>
-        <Stack spacing={4} w={"full"} maxW={"md"}>
-          <Heading color={"#205375"} fontSize={"2xl"}>
-            Sign Up
-          </Heading>
-          <form onSubmit={Register}></form>
-          <FormControl color={"#205375"} id="username">
-            <FormLabel>Username</FormLabel>
-            <Input
-              borderColor={"#205375"}
-              variant="outline"
-              color="teal"
-              placeholder="username"
-              _placeholder={{ color: "inherit" }}
-              // value={name}
-              // onChange={(e) => setName(e.target.value)}
-            />
-          </FormControl>
-          <FormControl color={"#205375"} id="email">
-            <FormLabel>Email</FormLabel>
-            <Input
-              borderColor={"#205375"}
-              variant="outline"
-              color="teal"
-              placeholder="email"
-              _placeholder={{ color: "inherit" }}
-              // value={email}
-              // onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormControl>
-          <FormControl color={"#205375"} id="password">
-            <FormLabel>Password</FormLabel>
-            <Input
-              borderColor={"#205375"}
-              variant="outline"
-              color="teal"
-              placeholder="password"
-              _placeholder={{ color: "inherit" }}
-              // value={password}
-              // onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
-
-          <Stack spacing={6}>
-            <Stack
-              direction={{ base: "column", sm: "row" }}
-              align={"start"}
-              justify={"space-between"}
-            ></Stack>
-            <Button colorScheme={"blue"} variant={"solid"}>
+    <Fragment>
+      {redirect && <Navigate to="/login" />}
+      <Stack
+        bg={"white"}
+        minH={"100vh"}
+        direction={{ base: "column", md: "row" }}
+      >
+        <Flex p={8} flex={1} align={"center"} justify={"center"}>
+          <Stack spacing={4} w={"full"} maxW={"md"}>
+            <Heading color={"#205375"} fontSize={"2xl"}>
               Sign Up
-            </Button>
-            <Text color={"#205375"} flex={"1"} align={"center"}>
-              Already have an account?
-            </Text>
-            <Link to="/login">
-              <Button colorScheme={"blue"} variant={"solid"}>
-                Sign In
+            </Heading>
+            <FormControl color={"#205375"} id="username">
+              <FormLabel>Username</FormLabel>
+              <Input
+                borderColor={"#205375"}
+                variant="outline"
+                color="teal"
+                placeholder="username"
+                _placeholder={{ color: "inherit" }}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </FormControl>
+            <FormControl color={"#205375"} id="email">
+              <FormLabel>Email</FormLabel>
+              <Input
+                borderColor={"#205375"}
+                variant="outline"
+                color="teal"
+                placeholder="email"
+                _placeholder={{ color: "inherit" }}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormControl>
+            <FormControl color={"#205375"} id="password">
+              <FormLabel>Password</FormLabel>
+              <Input
+                borderColor={"#205375"}
+                variant="outline"
+                color="teal"
+                placeholder="password"
+                _placeholder={{ color: "inherit" }}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormControl>
+
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              ></Stack>
+              <Button onClick={register} colorScheme={"blue"} variant={"solid"}>
+                Sign Up
               </Button>
-            </Link>
+              <Text color={"#205375"} flex={"1"} align={"center"}>
+                Already have an account?
+              </Text>
+              <Link to="/login">
+                <Button colorScheme={"blue"} variant={"solid"}>
+                  Sign In
+                </Button>
+              </Link>
+            </Stack>
           </Stack>
-        </Stack>
-      </Flex>
-      <Flex bg={"#205375"} flex={1}>
-        <Image
-          alt={"Login Image"}
-          objectFit={"cover"}
-          src={
-            "https://i.ibb.co/wMpR7ty/3d-flame-man-standing-next-to-red-phone-with-project-statistics-on-screen.png"
-          }
-        />
-      </Flex>
-    </Stack>
+        </Flex>
+        <Flex bg={"#205375"} flex={1}>
+          <Image
+            alt={"Login Image"}
+            objectFit={"cover"}
+            src={
+              "https://i.ibb.co/wMpR7ty/3d-flame-man-standing-next-to-red-phone-with-project-statistics-on-screen.png"
+            }
+          />
+        </Flex>
+      </Stack>
+    </Fragment>
   );
 }
